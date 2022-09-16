aa58f800317b519b7779ed1f9d5d43b004312631
ohno0722 <108727779+ohno0722@users.noreply.github.com>
2022-09-16 02:00:26 +0000

    devise追加途中

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,18 @@
 Rails.application.routes.draw do
-  devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+
+   devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
   sessions: "admin/sessions"
 }
 
+  namespace :admin do
+    get "/" => "homes#top"
+  end
+
+
+  scope module: :public do
+    root:to => "homes#top"
+
+  end
 
   devise_for :users,skip: [:passwords], controllers: {
   registrations: "public/registrations",
